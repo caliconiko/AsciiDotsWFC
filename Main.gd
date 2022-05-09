032578c4bfd7301fcaf0a46a5115901e69c0044f
PhotonNikko <69907559+photon-niko@users.noreply.github.com>
2022-05-09 21:53:17 +0700

    RUN!

--- Main.gd
+++ Main.gd
@@ -8,6 +8,12 @@ onready var asciidotsery = $AsciiDotsery
 
 onready var init_button = $HBoxContainer/VBoxContainer/InitButton
 onready var step_button = $HBoxContainer/VBoxContainer/StepButton
+onready var run_button = $HBoxContainer/VBoxContainer/HBoxContainer/RunButton
+onready var stop_button = $HBoxContainer/VBoxContainer/HBoxContainer/StopButton
+
+onready var run_timer = $RunTimer
+
+onready var interpreter_buttons = [step_button, run_button, stop_button]
 
 var ticks = 0
 
@@ -16,9 +22,11 @@ var DOT_COLOR = Color(0.976471, 0.14902, 0.447059)
 var wfc
 
 var do_magic = false
+var interpreting = false
 
 var dots = []
-var dot_counts = [0]
+var asciidot_ticks = 0
+var dot_count = -1
 
 export(int,0,90) var wave_function_width = 90
 export(int,0,25) var wave_function_height = 25
@@ -30,16 +38,12 @@ func _ready():
 	dot_count_label.modulate = DOT_COLOR
 	
 	wfc = WFC.new(Vector2(wave_function_width,wave_function_height),Global.module_data)
-	var s = "01234"
-	s[0]=""
-	s=s.insert(0, "asd")
-	print(s)
 
 func _process(_delta):
 	text_box.bbcode_text = highlight_dots(wfc.as_string())
 	ticks_label.text = str(ticks)
-	asciidots_ticks_label.text = str(len(dot_counts))
-	dot_count_label.text = str(dot_counts[-1])
+	asciidots_ticks_label.text = str(asciidot_ticks)
+	dot_count_label.text = str(dot_count)
 	if wfc.is_broken():
 		redo_magic()
 	
@@ -49,6 +53,8 @@ func _process(_delta):
 	else:
 		do_magic = false
 		init_button.disabled=false
+		
+
 
 func highlight_dots(wf_str:String):
 	var wf_lines = wf_str.split("\n")
@@ -62,6 +68,11 @@ func highlight_dots(wf_str:String):
 	var highlighted_lines_splitted = wf_lines_splitted.duplicate()
 	
 	for dot_pos in dots:
+		var dot_pos_below_zero = dot_pos.x<0 or dot_pos.y<0
+
+		if dot_pos_below_zero:
+			interpreting=false
+			return ""
 		var wf_char = wf_lines[dot_pos.y][dot_pos.x]
 		var highlighted_char = "[color=#"+DOT_COLOR.to_html(false)+"]"+wf_char+"[/color]"
 		
@@ -79,22 +90,52 @@ func highlight_dots(wf_str:String):
 	return combined_highlighted
 
 func redo_magic():
+	interpreting=false
 	dots=[]
-	dot_counts=[0]
-	init_button.disabled=true
-	step_button.disabled=true
+	dot_count = -1
+	asciidot_ticks = 0
 	ticks = 0
+	
+	init_button.disabled=true
+	for button in interpreter_buttons:
+		button.disabled = true
+		
 	wfc.initialize()
 	do_magic = true
+
+func step_interpreter():
+	asciidotsery.step()
+	dots=asciidotsery.get_dots()
+	dot_count=len(dots)
+	asciidot_ticks+=1
 
 func _on_GenerateButton_pressed():
 	redo_magic()
 
 func _on_InitButton_pressed():
+	dots=[]
+	dot_count = -1
+	asciidot_ticks = 0
 	asciidotsery.initialize(wfc.as_string())
-	step_button.disabled=false
-
+	for button in interpreter_buttons:
+		button.disabled = false
+	
 func _on_StepButton_pressed():
-	asciidotsery.step()
-	dots=asciidotsery.get_dots()
-	dot_counts.append(len(dots))
+	step_interpreter()
+
+func _on_RunButton_pressed():
+	interpreting=true
+	run_timer.start()
+
+func _on_RunTimer_timeout():
+	var dot_count_good = dot_count<0
+	if interpreting and dot_count<1000 or dot_count_good:
+		step_interpreter()
+		run_timer.start()
+	else:
+		interpreting = false
+	
+func _on_StopButton_pressed():
+	interpreting=false
+
+
